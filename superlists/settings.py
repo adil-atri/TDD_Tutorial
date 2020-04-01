024f0382c31dbbf0484a6e0553900f51e28ba5fd
Adil Assouab <assouab@atrihub.io>
2020-04-01 16:30:29 -0700

    feat(TDD Tutorial): Settings DB modification
    Modified DB in settings
    
    [ICTRAINING-384]

--- superlists/settings.py
+++ superlists/settings.py
@@ -14,6 +14,10 @@ import os
 import django_heroku
 import dj_database_url
 
+if 'HEROKU_APP' in os.environ:
+    # Activate Django-Heroku
+    django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,8 +81,21 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
+
+
+if 'HEROKU_APP' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travis_ci_test',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
